a53bb8d8601a9472e41ef2045d1ff46acd598e8d
zachurdev <168770225+zachurdev@users.noreply.github.com>
2024-05-06 10:09:37 -0500

    OK

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,8 +1,14 @@
+function supportWebsiteRedirect() {
+  window.location.href = 'https://portal.eosmedspa.com/'
+}
 function mainWebsiteRedirect() {
   window.location.href = 'https://www.eosmedspa.com/'
 }
 function bookingWebsiteRedirect() {
   window.location.href = 'https://dashboard.boulevard.io/booking/businesses/635ebb9a-061f-45e4-b142-ee990ba9557a/widget#/visit-type'
+}
+function clientLoginRedirect() {
+  window.location.href = 'https://blvd.app/@eosmedspa/login'
 }
 function clientPortalRedirect() {
   window.location.href = './client/'
@@ -12,10 +18,10 @@ function employeePortalRedirect() {
 }
 function employeeHandbookRedirect() {
     window.location.href = './handbook';
-  }
+}
 function supportTicketRedirect() {
     window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSfb4Wg247hoJ5gKBanIQSb_7ByuMYZ4KoTX7xZl5mGEitHa1w/viewform'
-  }
+}
 function employeeBookmarksRedirect() {
     window.location.href = './bookmarks'
-  }
+}
---
+++ mdbook/src/how-to/check-account-credit/index.md
@@ -0,0 +1,7 @@
+# Check Account Credit
+
+1. Go to Client Profile
+2. In the Profile Tabs, select "Payment Methods"
+3. Under the "Credits" section, any Account Credit will be listed as a dollar balance
+
+![Client Profile Payment Methods Tab](./image1.png)
